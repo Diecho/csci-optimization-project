e51ee67ad3e9ad2b267184e9d51b8ece57dd7a44
Diecho <d.hidalgo.pe@gmail.com>
2026-03-06 15:18:47 -0800

    person 1 done, and data analizys happy

--- cvs_reader.cc
+++ cvs_reader.cc
@@ -3,78 +3,76 @@
 #include <sstream>
 
 std::vector<Student> read_cvs(const std::string& filename) {
-    std::vector<Student> students;
-    std::ifstream file(filename);
-    std::string line;
+	std::vector<Student> students;
+	std::ifstream file(filename);
+	std::string line;
 
-    std::getline(file, line); 
+	std::getline(file, line); 
 
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string token;
-        Student s;
+	while (std::getline(file, line)) {
+		std::stringstream ss(line);
+		std::string token;
+		Student s;
 
-        s.is_valid = true;
+		s.is_valid = true;
 
-		for (size_t i = 0; i < 10000; i++) {
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			if(token ==  ""){
-				s.is_valid = false;
-				break;
-			}
-			s.gender= token;
-
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-
-			if(token == ""){
-				s.is_valid = false;
-				break;
-			}
-			s.attendance_rate= std::stod(token);
-
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-
-			if(token == ""){
-				s.is_valid = false;
-				break;
-			}
-			s.stress_index= std::stod(token);
-
-			std::getline(ss, token, ',');
-
-			if(token == ""){
-				s.is_valid = false;
-				break;
-			}
-			s.gpa = std::stod(token);
-
-
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-			std::getline(ss, token, ',');
-
-			if(token == ""){
-				s.is_valid = false;
-				break;
-			}
-			s.dropout= std::stoi(token);
-
-			if (s.is_valid) {
-				students.push_back(s);
-			}
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		if(token ==  ""){
+			s.is_valid = false;
+			break;
 		}
-    }
-    return students;
+		s.gender= token;
+
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+
+		if(token == ""){
+			s.is_valid = false;
+			break;
+		}
+		s.attendance_rate= std::stod(token);
+
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+
+		if(token == ""){
+			s.is_valid = false;
+			break;
+		}
+		s.stress_index= std::stod(token);
+
+		std::getline(ss, token, ',');
+
+		if(token == ""){
+			s.is_valid = false;
+			break;
+		}
+		s.gpa = std::stod(token);
+
+
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+		std::getline(ss, token, ',');
+
+		if(token == ""){
+			s.is_valid = false;
+			break;
+		}
+		s.dropout= std::stoi(token);
+
+		if (s.is_valid) {
+			students.push_back(s);
+		}
+	}
+	return students;
 }
--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    string filename = "student_dropout_dataset_v3_2500.csv";
+    string filename = "student_dropout_dataset_v3_10m.csv";
     
     cout << "Loading data from: " << filename << "...\n";
     
@@ -15,14 +15,76 @@ int main() {
     }
 
     cout << "Successfully loaded " << students.size() << " valid students!\n\n";
+	
+
+	int n_of_drop = 0;
+	int total_students = 0;
+	int non_drop_students = 0;	
+
+	int count_f = 0;
+	int count_m = 0;
+
+	double total_stress_drop = 0;
+	double total_stress_all = 0;
+	double total_stress_non = 0;
+
+	double total_gpa_drop = 0;
+	double total_gpa_all = 0;
+	double total_gpa_non = 0;
 
 
+	for(Student& st: students){
+		total_students++;	
+		total_stress_all+=st.stress_index;
+		total_gpa_all+=st.gpa;
+		if(st.dropout == 1){
+			++n_of_drop;
+			total_stress_drop+=st.stress_index;
+			total_gpa_drop+=st.gpa;
 
-    std::cout << "--- First Student Details ---\n";
-    std::cout << "Gender: " << students[0].gender << "\n";
-    std::cout << "GPA: " << students[0].gpa << "\n";
-    std::cout << "Stress Index: " << students[0].stress_index << "\n";
-    std::cout << "Dropout: " << students[0].dropout << "\n";
+			if(st.gender == "Male"){
+				++count_m;
+			}else {
+				++count_f;
+			}
+					
+		}	
+		else {
+			total_stress_non+=st.stress_index;
+			total_gpa_non+=st.gpa;
+			non_drop_students++;
+		}
+	}	
+	
+    cout << "--- Data ---\n";
+	
+    cout << "Total Number of student: " << total_students << "\n";
+    cout << "Total Number of student dropouts: " << n_of_drop << "\n";
+
+    cout << "Dropouts %: " << (static_cast<float>(n_of_drop) / total_students) * 100 << "\n";
+
+    cout << "------------------------------------------\n";
+
+    cout << "Male dropout n: " << count_m << "\n";
+    cout << "Female dropout n: " << count_f << "\n";
+
+    cout << "Male dropout %: " << (static_cast<float>(count_m) / n_of_drop) * 100 << "\n";
+    cout << "Female dropout %: " << (static_cast<float>(count_f) / n_of_drop) * 100 << "\n";
     
+    cout << "------------------------------------------\n";
+
+    cout << "Avg stress_index for a all student: " << total_stress_all / total_students << "\n";
+    cout << "Avg stress_index for a non-dropout student: " << total_stress_non / non_drop_students<< "\n";
+    cout << "Avg stress_index for a dropout student: " << total_stress_drop / n_of_drop << "\n";
+
+
+    cout << "------------------------------------------\n";
+
+    cout << "Avg GPA for a all student: " << total_gpa_all / total_students << "\n";
+    cout << "Avg GPA for a non-dropout student: " << total_gpa_non / non_drop_students<< "\n";
+    cout << "Avg GPA for a dropout student: " << total_gpa_drop / n_of_drop << "\n";
+
+    cout << "------------------------------------------\n";
     return 0;
 }
+
