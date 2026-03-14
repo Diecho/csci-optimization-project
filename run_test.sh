73a0907145c627c2964955863a24a2e73dd0d815
RandomGuy-001 <1017333@my.scccd.edu>
2026-03-14 01:42:12 -0700

    100% line tested

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,8 @@
 
 make gcov_build
 
-for i in {0..5}
+# ran an extra test that should fail when there is incorrect input file
+for i in {0..6}
 do
 	INPUT_FILE="test_$i.csv"
 	EXPECTED_FILE="cmp_test_$i.txt"
@@ -19,5 +20,14 @@ do
     fi
 done
 
-gcov -o . main.cc
+# this is for when there is no file, it will use default file
+./bin_gcov > "$ACTUAL_FILE"
+
+gcov -o bin_gcov-main main.cc
+# lcov --capture --directory . --output-file coverage.info --no-external
+# genhtml coverage.info --output-directory out_html
+
+# echo "Report generated! Open out_html/index.html in your browser."
+
+# gcov -o . main.cc
 
